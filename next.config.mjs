cccf036e288f650ee1da5e0537ced36816f8196e
AIPTU <anangp808@gmail.com>
2026-01-04 16:36:24 +0700

    fix...

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
 
 const nextConfig = {
-  output: "export",
-  basePath: repo ? `/${repo}` : "",
-  assetPrefix: repo ? `/${repo}/` : "",
-  images: {
-    unoptimized: true,
-  },
+	output: "export",
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
